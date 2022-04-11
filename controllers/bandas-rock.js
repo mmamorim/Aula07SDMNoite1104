5c6524c1adeea815d35aa3d4805e7c1827d30c53
mmamorim <marc.m.amorim@gmail.com>
2022-04-11 20:37:33 -0300

    adicionado rota bandas

--- controllers/bandas-rock.js
+++ controllers/bandas-rock.js
@@ -1,27 +1,40 @@
 import bandasRockDB from "../data/bandas-rock.js";
+import { Low, JSONFile } from 'lowdb'
+
+const adapter = new JSONFile("./banco.json")
+const db = new Low(adapter)
+db.read().then(function () {
+    console.log("banco carregado");
+}).catch(function (e) {
+    console.log(e);
+})
 
 const controller = {
 
   showList: function (req, res) {
-    res.status(200).json(bandasRockDB);
+    console.log(db.data);
+    res.status(200).json(db.data.bandasrock);
   },
   add: function (req, res) {
     console.log("recebi requisição...");
     let { id, name, country, style } = req.body;
-    bandasRockDB.bandasrock.data[id] = { id, name, country, style };
-    res.status(200).json(bandasRockDB);
+    db.data.bandasrock[id] = { id, name, country, style };
+    db.write();
+    res.status(200).json(db.data.bandasrock);
   },
   update: function (req, res) {
     console.log("recebi requisição update...", req.body);
     let { id, name, country, style } = req.body;
-    bandasRockDB.bandasrock.data[id] = { id, name, country, style };
-    res.status(200).json(bandasRockDB);
+    db.data.bandasrock[id] = { id, name, country, style };
+    db.write();
+    res.status(200).json(db.data.bandasrock);
   },
   remove: function (req, res) {
     console.log("recebi requisição remove...", req.body);
     let id = req.body.id;
-    delete bandasRockDB.bandasrock.data[id];
-    res.status(200).json(bandasRockDB);
+    delete db.data.bandasrock[id];
+    db.write();
+    res.status(200).json(db.data.bandasrock);
   },
 }
 
